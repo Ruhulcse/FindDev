9154897c9a01cd299a3972efbcf261e3dfea9676
Ruhulcse <ruhul.cse7862@gmail.com>
2020-12-13 13:34:16 +0600

    user register vaidation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
-//@route GET api/users
-//@desc TEST route
+const {check, validationResult } = require('express-validator');
+//@route POST api/users
+//@desc Register route
 //@access Public
 
-router.get('/',(req,res) => res.send('User route'));
-
+router.post('/',[
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Enter a valid email').isEmail(),
+    check('password','Password must be 6 digit').isLength({ min: 6})
+],
+(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array() });
+    }
+    res.send('User registered successfully..');
+});
 module.exports = router;
