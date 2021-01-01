cb284c4ddcc22856122cdcd2301eabd3cf23e76e
Ruhul amin <ruhul.cse7862@gmail.com>
2021-01-01 17:57:01 +0600

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json({ extended: false }));
 
 app.get('/',(req,res) => res.send('API Running..'));
 
-//Define Routers
+//Define Routers 
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/profile',require('./routes/api/profile'));
@@ -18,5 +18,5 @@ app.use('/api/posts',require('./routes/api/posts'));
 
 
 const PORT = process.env.PORT || 5000;
-
+//server checking
 app.listen(PORT, ()=> console.log(`server started on ${PORT}`));
