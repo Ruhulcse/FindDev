33e34346ed5f3bdcc077f2364ef6c23ee1befd2b
Ruhul amin <ruhul.cse7862@gmail.com>
2021-07-01 21:14:39 +0600

    Update server.js

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use('/api/posts',require('./routes/api/posts'));
 
 
 const PORT = process.env.PORT || 5000;
-//server check
+//server check devlopment project.
 app.listen(PORT, ()=> console.log(`server started on ${PORT}`));
